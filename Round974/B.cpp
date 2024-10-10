c64b16d081303479c479dd649e225ec46f9be258
自由的世界人 <3196812536@qq.com>
2024-10-10 18:19:11 +0800

    Round974B

--- Round974/B.cpp
+++ Round974/B.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int n, k;
+        std::cin >> n >> k;
+
+        int first = n - k + 1;
+        int odd_count = 0;
+
+        if (first % 2 == 0 && n % 2 == 0) {
+            odd_count = k / 2;
+        } else if (first % 2 == 1 && n % 2 == 1) {
+            odd_count = (k + 1) / 2;
+        } else {
+            odd_count = k / 2;
+        }
+
+        if (odd_count % 2 == 0) {
+            std::cout << "YES" << std::endl;
+        } else {
+            std::cout << "NO" << std::endl;
+        }
+    }
+}
